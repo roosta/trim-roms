442861a13fcac953a19020154b106dcb4e27eb9a
Daniel Berg <mail@roosta.sh>
2018-09-13 21:18:29 +0200

    Add cli opt --dry-run

--- src/clean.clj
+++ src/clean.clj
@@ -1,8 +1,12 @@
 (ns clean
   (:require [me.raynes.fs :as fs]
+            [clojure.tools.cli :refer [parse-opts]]
             [clojure.string :as str]))
 
 (def excludes #{"psx"})
+
+(def cli-options
+  [["-d" "--dry-run"]])
 
 (defn images
   [game]
@@ -13,21 +17,25 @@
     (fs/list-dir path)))
 
 (defn -main [& args]
+  (let [opts (parse-opts args cli-options)
+        base (first (:arguments opts))
+        parent (.getPath (fs/parent base))]
+    (if (fs/directory? base)
+      (doseq [sys (fs/list-dir base)]
 
-  (doseq [sys (fs/list-dir (first args))]
-
-    (let [backup-path (str "roms-cleaned/" (fs/name sys))]
-      (if (fs/exists? backup-path)
-        (println (str "Backup location already exist. Using: " backup-path))
-        (do
-          (fs/mkdirs backup-path)
-          (println (str "Created backup folder: " backup-path))))
-
-
-      (doseq [game (fs/glob sys "*.zip")]
-        (let [game-name (fs/name game)
-              image (filter #(str/includes? (fs/name %) game-name) (images game))]
-          (when (and (empty? image))
-            (when (fs/file? game)
-              (println "Going to delete" (.getPath game))
-              #_(fs/copy game (str backup-path "/" (fs/base-name game))))))))))
+        (let [backup-path (str parent "/roms-cleaned/" (fs/name sys))]
+          (doseq [game (fs/glob sys "*.zip")]
+            (let [game-name (fs/name game)
+                  image (filter #(str/includes? (fs/name %) game-name) (images game))]
+              (when (and (empty? image))
+                (when (fs/file? game)
+                  (let [target (fs/file (str backup-path "/" (fs/base-name game)))]
+                    (if (-> opts :options :dry-run)
+                      (println "Going to move" (.getPath game) "-->" (.getPath target))
+                      (do
+                        #_(fs/copy game target)
+                        #_(fs/delete game)
+                        (println "Moved" (.getPath game) "-->" (.getPath target))
+                        )))))))))
+      (binding [*out* *err*]
+        (println "Argument supplied does not appear to be a valid directory:" (first (:arguments opts)))))))
