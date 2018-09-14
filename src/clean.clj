4fec00193f4283408404334c9c1f09d029f532f5
Daniel Berg <mail@roosta.sh>
2018-09-14 19:56:08 +0200

    Remove unneeded when

--- src/clean.clj
+++ src/clean.clj
@@ -25,14 +25,13 @@
           (doseq [game (fs/glob sys "*.zip")]
             (let [game-name (fs/name game)
                   image (filter #(str/includes? (fs/name %) game-name) (images game))]
-              (when (and (empty? image))
-                (when (fs/file? game)
-                  (let [target (fs/file (str backup-path "/" (fs/base-name game)))]
-                    (if (-> opts :options :dry-run)
-                      (println "Going to move" (.getPath game) "-->" (.getPath target))
-                      (do
-                        (fs/copy+ game target)
-                        (fs/delete game)
-                        (println "Moved" (.getPath game) "-->" (.getPath target)))))))))))
+              (when (and (empty? image) (fs/file? game))
+                (let [target (fs/file (str backup-path "/" (fs/base-name game)))]
+                  (if (-> opts :options :dry-run)
+                    (println "Going to move" (.getPath game) "-->" (.getPath target))
+                    (do
+                      (fs/copy+ game target)
+                      (fs/delete game)
+                      (println "Moved" (.getPath game) "-->" (.getPath target))))))))))
       (binding [*out* *err*]
         (println "Argument supplied does not appear to be a valid directory:" (first (:arguments opts)))))))
